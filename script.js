3e6883ebab5bda76af74ef2e96da66fca3d917ac
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-11-03 16:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+const nameInput = document.getElementById('item-name-input')
+const priceInput = document.getElementById('item-price-input')
+const addButton = document.getElementById('add')
+let totalPrice = 0 
+function addToCart() {
+	const row = document.createElement('tr')
+	const name = document.createElement('td')
+	const price = document.createElement('td')
+	name.innerText = nameInput.value
+	price.innerText = priceInput.value
+	row.appendChild(name)
+	row.appendChild(price)
+	table.appendChild(row)     
+	console.log(table)
+	updateTotal()
+	nameInput.value = ''
+	priceInput.value = ''
+} 
+function updateTotal() {
+	totalPrice += parseInt(priceInput.value)
+	total.innerText = totalPrice
+}
+add.addEventListener('click',addToCart)
